2b7129fdcedbfceaaac50501b116be4a7c93e392
TassioNS9 <tassions@hotmail.com>
2025-10-20 10:30:17 -0300

    test: remove redundant comments from Loading component tests

--- src/components/loading/loading.spec.tsx
+++ src/components/loading/loading.spec.tsx
@@ -1,8 +1,6 @@
 import { render } from '@testing-library/react'
 import Loading from './loading.component'
 
-// Teste básico para o componente Loading
-// Verifica se a mensagem é exibida corretamente quando fornecida
 describe('Loading', () => {
   it('should show a message if there is one', () => {
     const { getByText } = render(<Loading message="Aguarde..." />)
