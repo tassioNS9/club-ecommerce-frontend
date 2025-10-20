e49966b70436b1cbffadf5930464106421c1cd12
TassioNS9 <tassions@hotmail.com>
2025-10-20 10:50:22 -0300

    test: ensure sign out, sign in and sign up button are correctly shown on header

---
+++ src/components/header/header.test.tsx
@@ -0,0 +1,52 @@
+import { renderWithRedux } from '../../helpers/test.helpers'
+import CartProduct from '../../types/cart.types'
+import Header from './header.component'
+
+describe('Header', () => {
+  it('should show sign out button if user is authenticated', () => {
+    const { getByText } = renderWithRedux(<Header />, {
+      preloadedState: { userReducer: { isAuthenticated: true } } as any
+    })
+
+    getByText('Sair')
+  })
+
+  it('should show sign in and sign up button if user is not authenticated', () => {
+    const { getByText } = renderWithRedux(<Header />, {
+      preloadedState: { userReducer: { isAuthenticated: false } } as any
+    })
+
+    getByText(/login/i)
+    getByText(/criar conta/i)
+  })
+
+  // Teste para verificar a contagem correta de produtos no carrinho
+  it('should show correct cart products count', () => {
+    const products: CartProduct[] = [
+      {
+        id: '1',
+        imageUrl: 'image_url',
+        name: 'Boné',
+        price: 100,
+        quantity: 10
+      },
+      {
+        id: '2',
+        imageUrl: 'image_url',
+        name: 'Jaqueta',
+        price: 100,
+        quantity: 12
+      }
+    ]
+
+    const { getByText } = renderWithRedux(<Header />, {
+      preloadedState: {
+        cartReducer: {
+          products
+        }
+      } as any
+    })
+
+    getByText('22')
+  })
+})
